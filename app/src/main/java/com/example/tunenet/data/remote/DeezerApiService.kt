d3f6c634479c8f29a818a6c8da098af11dd0b7f3
JoaquinSMV <187311141+JoaquinSMV@users.noreply.github.com>
2026-05-13 12:17:54 -0400

    fix: Corregir import de Retrofit y asegurar estructura de navegación

--- app/src/main/java/com/example/tunenet/data/remote/DeezerApiService.kt
+++ app/src/main/java/com/example/tunenet/data/remote/DeezerApiService.kt
@@ -2,7 +2,7 @@ package com.example.tunenet.data.remote
 
 import com.example.tunenet.data.model.DeezerSearchResponse
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
